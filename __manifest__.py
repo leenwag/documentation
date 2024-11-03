24e3713ad9a9c63441b8075dffad385c48034092
Leen Alghubari <97915430+leenwag@users.noreply.github.com>
2024-11-03 15:25:36 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'ISDT Integration',
+    'version': '1.0',
+    'category': 'Custom',
+    'description': 'Module for ISDT API Integration',
+    'depends': ['base'],
+    'data': [
+        'views/integration_data_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
